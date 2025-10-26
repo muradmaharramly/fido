0626c6ede5246e2e2834edc79649b5752a1741c3
muradmaharramly <mooradmaharramly@gmail.com>
2025-10-26 15:53:05 +0400

    edit

--- src/tools/store/store.js
+++ src/tools/store/store.js
@@ -3,6 +3,12 @@ import productReducer from "../reducers/productReducer";
 import newsReducer from "../reducers/newsReducer";
 import campaignReducer from "../reducers/campaignReducer";
 import adminReducer from "../reducers/administratorReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const enhancer =
+  process.env.NODE_ENV === 'development'
+    ? composeWithDevTools(applyMiddleware(...middleware))
+    : applyMiddleware(...middleware);
 
 const rootReducer = combineReducers({
     products: productReducer, 
